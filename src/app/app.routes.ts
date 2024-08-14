31ef65458ecefbf61fe422284daff120738d4c69
Leonardo Santos <leoleosantos21@gmail.com>
2024-08-14 17:40:33 -0300

    created about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { HomeComponent } from './components/home/home.component';
 export const routes: Routes = [
     {
         path: '',
-        title: "Premium Barbar",
+        title: "Premium Barber",
         component: HomeComponent,
         pathMatch: "full"
     },
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeroComponent } from '../hero/hero.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { HeroComponent } from '../hero/hero.component';
   imports: [
     NavbarComponent,
     HeroComponent,
+    AboutComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
