bfc8bfccd99de89c0444a1ac08b687ad194b4388
Leonardo Santos <leoleosantos21@gmail.com>
2024-08-14 17:46:41 -0300

    servicos Component criado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { HeroComponent } from '../hero/hero.component';
 import { AboutComponent } from '../about/about.component';
+import { ServicosComponent } from '../servicos/servicos.component';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ import { AboutComponent } from '../about/about.component';
     NavbarComponent,
     HeroComponent,
     AboutComponent,
+    ServicosComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
