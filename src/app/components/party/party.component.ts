dc12e2d1f48aa03930b968dbf4271d0e1845c63d
Leonardo Santos <leoleosantos21@gmail.com>
2024-08-15 19:15:11 -0300

    barbers ok

--- src/app/components/party/party.component.ts
+++ src/app/components/party/party.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { BarberComponent } from './components/barber/barber.component';
 
 @Component({
   selector: 'app-party',
   standalone: true,
-  imports: [],
+  imports: [
+    BarberComponent
+  ],
   templateUrl: './party.component.html',
   styleUrl: './party.component.scss'
 })
