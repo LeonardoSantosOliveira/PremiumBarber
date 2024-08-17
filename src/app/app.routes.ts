6f09325824729aaddaae90a89e111305e4775de0
Leonardo Santos <leoleosantos21@gmail.com>
2024-08-17 00:27:14 -0300

    modal component created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,20 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 export const routes: Routes = [
     {
         path: '',
+        redirectTo: 'home',
+        pathMatch: "full",
+    },
+    {
+        path: 'home',
         title: "Premium Barber",
         component: HomeComponent,
-        pathMatch: "full"
+    },
+    {
+        path: 'modal',
+        component: ModalComponent,
     },
 ];
