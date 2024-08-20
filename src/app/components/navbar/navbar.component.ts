2e6602ad9adbc09915bb0440b4e36cc60eba55dc
Leonardo Santos <leoleosantos21@gmail.com>
2024-08-20 18:01:33 -0300

    overlay menu add

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -13,17 +13,20 @@ import { ButtonComponent } from '../button/button.component';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
-
-  abrirMenu(menu: HTMLDivElement) {
+  abrirMenu(menu: HTMLDivElement, overlay: HTMLDivElement) {
     menu.classList.remove("hidden");
     menu.classList.remove("animate__slideOutRight");
     menu.classList.add("animate__slideInRight");
+
+    overlay.classList.remove("hidden");
+    overlay.style.zIndex = "0";
   }
 
-  fecharMenu(menu: HTMLDivElement) {
+  fecharMenu(menu: HTMLDivElement, overlay: HTMLDivElement) {
     menu.classList.remove("animate__slideInRight");
     menu.classList.add("animate__slideOutRight");
-  }
 
+    overlay.classList.add("hidden");
+    overlay.style.zIndex = "-1";
+  }
 }
