e9367d3f33699882fe19ea945547731ff7b07547
Leonardo Santos <leoleosantos21@gmail.com>
2024-08-20 17:57:06 -0300

    menu done

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -14,4 +14,16 @@ import { ButtonComponent } from '../button/button.component';
 })
 export class NavbarComponent {
 
+
+  abrirMenu(menu: HTMLDivElement) {
+    menu.classList.remove("hidden");
+    menu.classList.remove("animate__slideOutRight");
+    menu.classList.add("animate__slideInRight");
+  }
+
+  fecharMenu(menu: HTMLDivElement) {
+    menu.classList.remove("animate__slideInRight");
+    menu.classList.add("animate__slideOutRight");
+  }
+
 }
